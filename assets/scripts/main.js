23ae7aec5cb23a9f24c51b6523ec13209d6f13c2
BrunoMazeto4 <brunosantosmazeto@gmail.com>
2025-10-18 20:10:31 -0300

    update script file

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,15 +1,19 @@
-window.addEventListener("load", () => {
-  const audio = document.getElementById("music")
-  const theme = document.body.classList[0]
-  const music =
-    theme === "light-theme" ? "normal-world.mpeg" : "inverted-world.mpeg"
+const audio = document.getElementById("music")
 
+window.addEventListener("click", function () {
   audio.play()
   audio.volume = 0.2
-  audio.src = `assets/musics/${music}`
 })
 
 function switchTheme() {
   document.body.classList.toggle("dark-theme")
   document.body.classList.toggle("light-theme")
+
+  const theme = document.body.classList[0]
+  const music =
+    theme === "light-theme" ? "normal-world.mpeg" : "inverted-world.mpeg"
+
+  audio.src = `assets/musics/${music}`
+  audio.play()
+  audio.volume = 0.2
 }
