32132bcc721d0852dd256f4bd7e37664b93aac82
BrunoMazeto4 <brunosantosmazeto@gmail.com>
2025-10-18 19:53:00 -0300

    update script file

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,13 +1,13 @@
 function switchTheme() {
   document.body.classList.toggle("dark-theme")
   document.body.classList.toggle("light-theme")
-
-  const theme = document.body.classList[0]
-  const music =
-    theme === "light-theme" ? "normal-world.mpeg" : "inverted-world.mpeg"
-
-  const audio = document.getElementById("music")
-  audio.src = `assets/musics/${music}`
-  audio.play()
-  audio.volume = 0.2
 }
+
+const theme = document.body.classList[0]
+const audio = document.getElementById("music")
+const music =
+  theme === "light-theme" ? "normal-world.mpeg" : "inverted-world.mpeg"
+
+audio.src = `assets/musics/${music}`
+audio.play()
+audio.volume = 0.2
